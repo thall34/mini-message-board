e63b9779864a1ed3410690a61041ec3ef2568825
Tyler <209157553+thall34@users.noreply.github.com>
2026-05-27 09:48:16 -0400

    Add message validation and errors.ejs for display

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,15 @@
 const db = require('../db/queries');
+const { body, validationResult, matchedData } = require('express-validator');
+
+const validateMessage = [
+    body('messageName').trim()
+    .notEmpty().withMessage('Name is required.')
+    .isAlphanumeric().withMessage('Name must be alphanumeric (abc123).'),
+    body('messageText').trim()
+    .notEmpty().withMessage('Message text is required.')
+    .isLength({ max: 100 }).withMessage('Message text can only be 100 characters max.')
+    .escape(),
+];
 
 async function displayMessages(req, res) {
     const messages = await db.getAllMessages();
@@ -9,13 +20,22 @@ function displayForm(req, res) {
     res.render('form');
 };
 
-async function addMessage(req, res) {
-    const messageText = req.body.messageText;
-    const messageName = req.body.messageName;
+const addMessage = [
+    validateMessage,
+    async (req, res) => {
+        const errors = validationResult(req);
 
-    await db.addMessage(messageText, messageName);
-    res.redirect('/');
-};
+        if (!errors.isEmpty()) {
+            return res.status(400).render('form', {
+                errors: errors.array(),
+            });
+        }
+
+        const { messageText, messageName } = matchedData(req);
+        await db.addMessage(messageText, messageName);
+        res.redirect('/');
+    }
+];
 
 async function displayMessageDetails(req, res) {
     const id = req.params.id;
